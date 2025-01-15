b7fdfb53f1beb58b6f51e53feb521b56a75543fa
khrystal <kdb23bj@yahoo.com>
2025-01-15 03:47:15 -0500

    Updates

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,18 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    public_users.post("/register", (req,res) => {
+        const { username, password } = req.body;
+        if (!username || !password) {
+          return res.status(400).json({ message: "Username and password are required" });
+        }
+        if (users.find((user) => user.username === username)) {
+          return res.status(409).json({ message: "Username already exists" });
+        }
+        users.push({ username, password });
+        return res.status(201).json({ message: "User registered successfully" });
+    });
+    
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -24,12 +33,6 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
  });
   
-// Get book details based on author
-//public_users.get('/author/:author',function (req, res) {
-   //const getAuthor = req.params.author.keys;
-   //res.send(getAuthor);
-  //return res.status(300).json({message: "Yet to be implemented"});
-///});
 
 public_users.get("/author/:author", function (req, res) {
     const authorName = req.params.author;
@@ -66,8 +69,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    const reviews = books[isbn].reviews;
+    return res.status(200).send(reviews);
+  } else {
+    return res.status(404).json({ message: "Book not found with the provided ISBN" });
+  }
 });
+  
 
 module.exports.general = public_users;
